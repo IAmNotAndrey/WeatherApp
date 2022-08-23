8dd945bc1b4d654a8b018c9361adcac423b6c4a6
JeNeSuisPasAndrew <permandrey2000@mail.ru>
2022-08-23 15:32:14 +0500

    Создание приложения (демо)

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -0,0 +1,135 @@
+package com.example.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText user_field;
+    private Button main_btn;
+    private TextView result_info;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        user_field = findViewById(R.id.user_field);
+        main_btn = findViewById(R.id.main_btn);
+        result_info = findViewById(R.id.result_info);
+
+        main_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (user_field.getText().toString().trim().equals(""))
+                    Toast.makeText(MainActivity.this, R.string.no_user_input, Toast.LENGTH_SHORT).show();
+                else {
+                    String city = user_field.getText().toString().trim();
+                    String key = "ff8445b50ba6f46af1e4cc158f78dc64";
+                    String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key + "&units=metric&lang=ru";
+
+                    new GetUrlData().execute(url);
+                }
+            }
+        });
+    }
+
+    private class GetUrlData extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            result_info.setText(R.string.wait);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(strings[0]);
+                connection = (HttpURLConnection)url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null)
+                    buffer.append(line).append("\n");
+
+                return buffer.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null)
+                    connection.disconnect();
+
+                try {
+                    if (reader != null)
+                        reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @SuppressLint("SetTextI18n")
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                JSONObject main = jsonObject.getJSONObject("main");
+
+                JSONArray weather_array = (JSONArray) jsonObject.get("weather");
+                JSONObject weather = (JSONObject) weather_array.get(0);
+
+                String rDescription = weather.getString("description");
+                String description = rDescription.substring(0, 1).toUpperCase() + rDescription.substring(1);
+                double temp = main.getDouble("temp");
+                double feels_like = main.getDouble("feels_like");
+                double pressure = 0.75 * main.getInt("pressure");
+
+                result_info.setText(
+                        description + "\n" +
+                        "Температура: " + temp + "\n" +
+                        "Чувствуется как: " + feels_like + "\n" +
+                        "Давление: " + pressure + " мм рт ст" + "\n"
+                );
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
